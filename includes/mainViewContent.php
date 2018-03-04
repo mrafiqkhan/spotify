397c8eb02cbd5d6bc56557deb3251a946a665180
mrafiq987@gmail.com <mrafiq987@gmail.com>
2018-03-04 19:30:40 +0500

    initial commit

---
+++ includes/mainViewContent.php
@@ -0,0 +1,27 @@
+<div id="mainViewContent">
+  <div id="mainContent">
+    <h1 id="pageHeadingBig">Subscribe Now and Listen music around the world</h1>
+    <div id="gridViewContainer">
+      <?php
+
+
+      $albumQuery = mysqli_query($con, "SELECT * FROM albums LIMIT 12");
+
+      while($row = mysqli_fetch_assoc($albumQuery)){ ?>
+
+        <div class="album">
+          <div class="albumHeader">
+            <img src="<?php echo $row['artworkPath'] ?>" alt="">
+          </div>
+          <div class="albumFooter">
+            <p><?php echo $row['title']; ?></>
+            <p><?php echo $row['artist']; ?></p>
+          </div>
+        </div>
+
+      <?php  }  ?>
+
+
+    </div>
+  </div>
+</div>
