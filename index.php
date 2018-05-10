c9f3d342418e29c75a63572081562f4aab1ad750
mrafiqkhan <mrafiq987@gmail.com>
2018-05-10 17:10:22 +0500

    modified index.php

--- index.php
+++ index.php
@@ -2,9 +2,5 @@
  <link rel="stylesheet" href="assets/css/index.css">
 <?php include("includes/header.php"); ?>
 <?php include("includes/mainViewContent.php");?>
-
-
-
-
 <?php include("includes/footerTop.php"); ?>
 <?php include("includes/footer.php"); ?>
